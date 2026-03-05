4b03d477b0b3ed39ee762e689e8c8128083014ef
Sebastian Dang <85420923+inf7m@users.noreply.github.com>
2026-03-05 11:57:51 +0800

    Create config.py
    push

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+# Folder configuration
+INPUT_FOLDER = "input_images"
+OUTPUT_FOLDER = "output_images"
+
+# Image radius processing settings
+BLUR_RADIUS = 2
+
+# Default number of workers for parallel
+DEFAULT_WORKERS = 2
